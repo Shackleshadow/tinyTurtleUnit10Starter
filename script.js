95b7ace6d8bdb5e2d0315140d3ddb4485f36ff53
Bryan Huang <chesschamp617@gmail.com>
2017-03-22 14:09:29 +0000

    TinyTurtleV2

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
 function square(){
-forward(50);
+forward(100);
 right(90);
-forward(50);
+forward(100);
 right(90);
-forward(50);
+forward(100);
 right(90);
-forward(50);
+forward(100);
 right(90);
-forward(50);
+forward(100);
 }
 
+function triangle(size){
+    forward(size);
+    right(120);
+    forward(size);
+    right(120);
+    forward(size);
+    right(120);
+}
+
+function rectangle(size){
+left(90);
+forward(100);
+left(90);
+forward(200);
+left(90);
+forward(100);
+left(90);
+forward(200);
+}
 // Type your function call below
-
-
+square();
+rectangle();
+triangle(80);
 stamp();
